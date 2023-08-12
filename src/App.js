b6ab39d933f352a8aabc1bed1daf76d1ac81abcd
Amanullah Aman <amancse.aiub@gmail.com>
2023-08-12 20:40:56 +0600

    apply custom hook crate new task

--- src/App.js
+++ src/App.js
@@ -6,31 +6,27 @@ import useHttp from "./Hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const taskTrasnform = (taskObj) => {
-    const loadedTasks = [];
 
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: "https://react-http-request-test-12f89-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
-    },
-    taskTrasnform
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
+    const taskTrasnform = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
     console.log("use effect fetch task initial");
-    fetchTasks();
-  }, []);
+    fetchTasks(
+      {
+        url: "https://react-http-request-test-12f89-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+      },
+      taskTrasnform
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     console.log("add task loading");
